2ab812a26699aca870d85b6a11dc116be88efccc
Michael Arnaldi <arnaldimichael@gmail.com>
2021-06-22 12:19:19 +0100

    has bug

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 sealed trait IO[R, E, A]
 
 class Tag[A]()
+class Has[A]()
 
 enum Exit[E, A]:
   case Fail(e: E)
@@ -10,14 +11,14 @@ extension [R, E, A](io: IO[R, E, A])
   def map[B](f: A => B): IO[R, E, B] = ???
   def flatMap[R2, E2, B](f: A => IO[R2, E2, B]): IO[R2 & R, E2 | E, B] = ???
   def foldM[R2, E2, A2, R3, E3, A3](f: A => IO[R2, E2, A2], g: E => IO[R3, E3, A3]): IO[R3 & R2 & R, E3 | E2, A3 | A2] = ???
-  def inject[R2, R3](r: R2)(implicit tag: Tag[R2], ev: R2 & R3 => R): IO[R3, E, A] = ???
+  def inject[R2, R3](r: R2)(implicit tag: Tag[R2], ev: Has[R2] & R3 => R): IO[R3, E, A] = ???
   def catchAll[R2, E2, A2](f: E => IO[R2, E2, A2]) = ???  
   def catchSome[R2, E2, A2, E3, E4](f: PartialFunction[E, (E3, IO[R2, E2, A2])])(implicit ev: E => E3 | E4): IO[R & R2, E2 | E4, A | A2] = ???
 
 object IO {
   def succeed[A](a: => A): IO[Any, Nothing, A] = ???
   def fail[E](e: => E): IO[Any, E, Nothing] = ???
-  def use[R, R2, E2, A](f: R => IO[R2, E2, A])(implicit tag: Tag[R]): IO[R & R2, E2, A] = ???
+  def use[R, R2, E2, A](f: R => IO[R2, E2, A])(implicit tag: Tag[R]): IO[Has[R] & R2, E2, A] = ???
 }
 
 trait Console {
@@ -45,5 +46,5 @@ val program = for {
   _ <- IO.use((T: Console) => T.log(s"z: ${z}"))
 } yield ()
 
-// CORRECT AS IO[Console, Nothing, Unit]
+// BUGGED AS IO[Nothing, Nothing, Unit]
 val main = program inject Math.Live
